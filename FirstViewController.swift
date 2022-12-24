db7b0a34f086ccba8639c31a6c2674e933e8103f
aestheticmay <60157413+aestheticmay@users.noreply.github.com>
2022-12-24 22:44:13 +0300

    Initial commit

---
+++ FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  GCD
+//
+//  Created by Майя Калицева on 24.12.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Майя Калицева on 24.12.2022.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
